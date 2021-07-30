222e1ce964755628d330f63cda110e9d1e36fcf1
camilo Rodriguez <camilo.rodriguez@sinco.com.co>
2021-07-29 21:26:55 -0500

    configuracion incial proyecto

---
+++ src/wellsolutions/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = True 
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+INSTALLED_APPS = DJANGO_APPS + APPS_THIRD_PARTY + APPS
+
+
+
