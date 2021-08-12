c413cdd49564f7d1b9b0ef7c7ea58e0c5f868827
Yeison Rodriguez <yrrodriguezb@gmail.com>
2021-08-12 09:36:26 -0500

    Se ajustan imagenes de servicios, secciones de quienes somos y la configuracion del correo y su template

--- src/wellsolutions/settings/prod.py
+++ src/wellsolutions/settings/prod.py
@@ -9,8 +9,8 @@ INSTALLED_APPS = DJANGO_APPS + APPS_THIRD_PARTY + APPS
 # Email
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = environ.get("EMAIL_HOST", None)
 EMAIL_HOST_USER = environ.get("EMAIL_HOST_USER", None)
 EMAIL_HOST_PASSWORD = environ.get("EMAIL_HOST_PASSWORD", None)
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
+EMAIL_PORT = environ.get("EMAIL_PORT", None)
+EMAIL_USE_TLS = environ.get("EMAIL_USE_TLS", True)
